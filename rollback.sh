f7aec71c84f7546364ce6dca3662c5f9a853743c
Vokhmin Alexey V <avokhmin@gmail.com>
2012-12-12 17:00:56 +0400

    remove src.rpm & rpm from repository on rollback

--- rollback.sh
+++ rollback.sh
@@ -12,6 +12,10 @@ echo "RELEASED = $released"
 echo "REPOSITORY_NAME = $rep_name"
 echo "ARCH = $arch"
 
+# Container path:
+# - /home/vagrant/container
+container_path=/home/vagrant/container
+
 repository_path=/home/vagrant/share_folder/repository
 srpms_rep_path=$repository_path/SRPMS/$rep_name/release
 rpms_rep_path=$repository_path/$arch/$rep_name/release
@@ -34,4 +38,11 @@ if [ -d "$rpms_rep_path/$m_info_folder-backup" ]; then
   rm -rf "$rpms_rep_path/$m_info_folder"
   mv "$rpms_rep_path/$m_info_folder-backup" $rpms_rep_path/$m_info_folder
 fi
+
+for file in $( ls -1 $container_path/SRC_RPM ) ; do
+  rm $srpms_rep_path/$file
+done
+for file in $( ls -1 $container_path/RPM ) ; do
+  rm $rpms_rep_path/$file
+done
 exit 0
