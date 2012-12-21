6cb6df7f601907b56a365a84cd98e4c386b8f89d
Vokhmin Alexey V <avokhmin@gmail.com>
2012-12-21 19:40:50 +0400

    add sign_rpm.sh script

---
+++ sign_rpm.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+echo '--> publish-build-list-script: sign_rpm.sh'
+
+rpm_path="$RPM_PATH"
+
+gnupg_path=/home/vagrant/.gnupg
+if [ ! -d "$gnupg_path" ]; then
+  echo "--> $gnupg_path does not exist"
+  exit 0
+fi
+
+keyname=`gpg --with-fingerprint $gnupg_path/secring.gpg | sed -n 1p | awk '{ print $2 }' | awk '{ sub(/.*\//, ""); print }'`
+rpm --addsign $rpm_path --define="_gpg_path ~/.gnupg" --define="_gpg_name $keyname"
+
+exit 0
