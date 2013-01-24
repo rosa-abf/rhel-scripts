097453954ce4835d71b609749600ccc11a66c351
Vokhmin Alexey V <avokhmin@gmail.com>
2013-01-24 20:13:14 +0400

    refactoring

--- sign_rpm.sh
+++ sign_rpm.sh
@@ -12,7 +12,7 @@ fi
 # Hot fix
 # TODO: Fix me!!!
 # see: http://file-store.rosalinux.ru/api/v1/file_stores/6af8c79d307b437a56a01b031e052b88b1d310d8.log?show=true
-cp -rf /home/vagrant/.gnupg /root/.gnupg
+cp -rf $gnupg_path /root/.gnupg
 
 keyname=`gpg --with-fingerprint $gnupg_path/secring.gpg | sed -n 1p | awk '{ print $2 }' | awk '{ sub(/.*\//, ""); print }'`
 rpm -vv --  $rpm_path --define="_gpg_path $gnupg_path" --define="_gpg_name $keyname" --define="_signature gpg" --define="_gpgbin /usr/bin/gpg"
