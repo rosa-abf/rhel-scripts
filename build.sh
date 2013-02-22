3b860abff940eccbca5c15b35ebd17ac8d28c00b
Vokhmin Alexey V <avokhmin@gmail.com>
2013-02-22 23:31:20 +0400

    remove spaces

--- build.sh
+++ build.sh
@@ -131,29 +131,29 @@ if [ "$distrib_type" == 'mdv' ] ; then
 else
   echo '
 config_opts["yum.conf"] = """
-  [main]
-  cachedir=/var/cache/yum
-  debuglevel=1
-  reposdir=/dev/null
-  logfile=/var/log/yum.log
-  retries=20
-  obsoletes=1
-  gpgcheck=0
-  assumeyes=1
-  syslog_ident=mock
-  syslog_device=
+[main]
+cachedir=/var/cache/yum
+debuglevel=1
+reposdir=/dev/null
+logfile=/var/log/yum.log
+retries=20
+obsoletes=1
+gpgcheck=0
+assumeyes=1
+syslog_ident=mock
+syslog_device=
 
-  # repos
+# repos
   ' >> $default_cfg
   while read CMD; do
     name=`echo $CMD | awk '{ print $1 }'`
     url=`echo $CMD | awk '{ print $2 }'`
     echo "
-  [$name]
-  name=$name
-  enabled=1
-  baseurl=$url
-  failovermethod=priority
+[$name]
+name=$name
+enabled=1
+baseurl=$url
+failovermethod=priority
 
     " >> $default_cfg
   done < $media_list
