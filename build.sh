6b8c8e1601a1f0e273c3c25a2c735365a5f7826a
Alex Burmashev <alex.burmashev@rosalab.ru>
2013-02-25 16:52:59 +0400

    added check for platform type to rpm -qa

--- build.sh
+++ build.sh
@@ -169,7 +169,9 @@ for arch in $arches ; do
 
   # Check exit code
   if [ $rc != 0 ] ; then
-    rpm -qa |grep genhdlist2
+  if [ "$platform_type" == 'mdv' ] ; then
+  rpm -qa |grep genhdlist2
+  fi  
     break
   fi
 
