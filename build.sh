464a63290ca9f8d2a5ea7182bfd67de8fb724005
Alex Burmashev <alex.burmashev@rosalab.ru>
2013-02-28 13:23:13 +0400

    fixed path

--- build.sh
+++ build.sh
@@ -198,7 +198,7 @@ src_rpm_name=`ls -1 | grep 'src.rpm$'`
 echo '--> Building rpm...'
 if [ "$distrib_type" == 'mdv' ] ; then
   echo Checking what is inside buildir
-  tree $chroot_path/builddir/build/ >> $results_path/builddir-tree.log
+  tree $chroot_path/builddir/ >> $results_path/builddir-tree.log
 fi
 $mock_command $src_rpm_name --resultdir $rpm_path -v --no-cleanup-after --no-clean
 # Save exit code
