95bbc5f61c1cd7d314b664482d01f49532b847c9
Vokhmin Alexey V <avokhmin@gmail.com>
2013-04-23 13:21:44 +0400

    #5: rollback changes

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -2,7 +2,5 @@
 
 sudo yum install -y tree nfs-utils
 sudo /bin/bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
-sudo /bin/bash -c 'echo "185.4.234.68 file-store.rosalinux.ru" >> /etc/hosts'
-sudo /bin/bash -c 'echo "195.19.76.241 abf.rosalinux.ru" >> /etc/hosts'
 
 exit 0
