8a79dc6b86943942d8762d0028b989d2daca4e3c
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-15 19:57:30 +0400

    install mock package

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sudo yum install -y tree git rpm
+sudo yum install -y tree git rpm mock
 sudo /bin/bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 
 sudo usermod -a -G vboxsf vagrant
