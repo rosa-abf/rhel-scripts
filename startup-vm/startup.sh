ce5be3e61d21597e3ffb658a38daf706fa56c0bf
Vokhmin Alexey V <avokhmin@gmail.com>
2013-10-15 20:37:08 +0400

    #25: added vagrant into mock group

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -4,5 +4,6 @@ sudo yum install -y tree git rpm mock ruby
 sudo /bin/bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 
 # sudo usermod -a -G vboxsf vagrant
+sudo usermod -a -G  mock vagrant
 
 exit 0
