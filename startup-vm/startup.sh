658055311dc89cf4bbbfc5a3a32b992ea919ca94
Vokhmin Alexey V <avokhmin@gmail.com>
2014-02-07 18:46:53 +0400

    Configure proxy

--- startup-vm/startup.sh
+++ startup-vm/startup.sh
@@ -6,4 +6,9 @@ sudo /bin/bash -c 'echo "nameserver 8.8.8.8" > /etc/resolv.conf'
 # sudo usermod -a -G vboxsf vagrant
 sudo usermod -a -G  mock vagrant
 
+# ABF_DOWNLOADS_PROXY, see: /etc/profile
+if [ "$ABF_DOWNLOADS_PROXY" != '' ] ; then
+  sudo /bin/bash -c "echo 'export http_proxy=$ABF_DOWNLOADS_PROXY' >> /etc/profile"
+fi
+
 exit 0
